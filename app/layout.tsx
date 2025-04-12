085380e7b8be8fc00f1c679201251390c2e0a729
webcoderspeed <webcoderspeed@gmail.com>
2025-04-12 19:00:48 +0530

    feat: analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
+import { Analytics } from '@vercel/analytics/react';
  
 const inter = Inter({ subsets: ["latin"] })
 
@@ -27,6 +28,7 @@ export default function RootLayout({
 				className={inter.className}
 				suppressHydrationWarning
 			>
+				<Analytics />
 				<ThemeProvider
 					attribute='class'
 					defaultTheme='light'
